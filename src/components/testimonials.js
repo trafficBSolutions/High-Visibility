25988900fcc56eb803dfd694dba19847af6da70f
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-07-18 18:22:37 -0400

    Add files via upload

---
+++ src/components/testimonials.js
@@ -0,0 +1,18 @@
+const Testimonials = [
+    {
+        name: "Willy",
+        review: "made my 911 look like new! The ceramic coating a year and a half still beads up like when it was applied. Highly recommend your service.",
+        date: "2023-06-15",
+    },
+    {
+        name: "D. Parker",
+        review: "Outstanding!  So satisfied by Benjamin’s attention to satisfying a customer’s needs. He was prompt, polite and very business-like. It was refreshing to see someone committed to his job and his focus on quality. …At a reasonable price!Highly recommended! Can’t say enough good things about his quality of work and good work ethic.",
+        date: "2022-03-11",
+    },
+    {
+        name: "Tommy Lamb",
+        review: "Benjamin and high visibility detailing did a superb job on all three of our vehicles. The ceramic wax is unbelievable!  Highly recommend their services and attention to detail.",
+        date: "2022-02-11",
+    }
+]
+export default Testimonials;
