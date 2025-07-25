25d2ec18fb56874beaab42803d8846eb045ed387
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-07-25 16:18:18 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,7 @@
 import Home from "./home";
-export {Home};
+import Error from "./error";
+import Services from "./services";
+import Testimonials from "./testimonials";
+import ContactUs from "./contact-us";
+import ServiceQuote from "./services/service-quote"
+export {Home, Error, Services, Testimonials, ContactUs, ServiceQuote};
